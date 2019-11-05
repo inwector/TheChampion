c841fb84bc5923680a02202d17bcca579ca3e0ff
İzzet Büyükkürkçü <ibuyukkurkcu@arti-soft.com.tr>
2019-11-05 15:28:05 +0300

    base

---
+++ TheChampion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheChampion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
